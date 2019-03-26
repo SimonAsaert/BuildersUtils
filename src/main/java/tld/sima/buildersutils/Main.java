105d7cf41930f2fc66120e8b8c92aff0833c55e1
SimonA <simon.asaert@gmail.com>
2019-03-26 15:34:57 +0000

    - Init

---
+++ src/main/java/tld/sima/buildersutils/Main.java
@@ -0,0 +1,17 @@
+package tld.sima.buildersutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
